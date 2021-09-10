b36cab82a0eefefc0147e82cdcf6766095ae1c02
Avillameza <villa.k.ana@gmail.com>
2021-09-09 21:32:18 -0700

    Init

---
+++ lib/villaana_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "villaana_view_tool/version"
+require_relative "villaana_view_tool/renderer"
+
+module VillaanaViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
